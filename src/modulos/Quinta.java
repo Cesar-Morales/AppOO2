836b0f0fea3e40ddec02739db9d21bcd29dbef07
Cesar-Morales <moraleescesar@gmail.com>
2021-11-20 19:24:40 -0300

    App

---
+++ src/modulos/Quinta.java
@@ -0,0 +1,17 @@
+package modulos;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Quinta {
+
+	private Map<String,Espacio> map = new HashMap<String, Espacio>();
+	EpocaDelAnio epoca;
+	Set tiposEspacios;
+	
+	public Quinta() {
+		
+	}
+	
+}
