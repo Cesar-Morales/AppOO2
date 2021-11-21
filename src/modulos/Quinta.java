d0424b023c8cb802400ed3e6b7f2a5b72c3b3774
Cesar-Morales <moraleescesar@gmail.com>
2021-11-20 21:51:41 -0300

    probando rama nueva

--- src/modulos/Quinta.java
+++ src/modulos/Quinta.java
@@ -1,17 +1,48 @@
 package modulos;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Quinta {
 
-	private Map<String,Espacio> map = new HashMap<String, Espacio>();
-	EpocaDelAnio epoca;
-	Set tiposEspacios;
+	private Map<String,Espacio> espacios;
+	private EstacionDelAnio estacion;
+	private Set<Espacio> tiposEspacios; 
 	
 	public Quinta() {
-		
+		setEspacios(new HashMap<String, Espacio>());
+		setEstacion(new EstacionDelAnio());
+		setTiposEspacios(new HashSet<Espacio>());
 	}
 	
+	/* Getters and Setters */
+
+	public Map<String,Espacio> getEspacios() {
+		return espacios;
+	}
+
+	public void setEspacios(Map<String,Espacio> espacios) {
+		this.espacios = espacios;
+	}
+
+	public EstacionDelAnio getEstacion() {
+		return estacion;
+	}
+
+	public void setEstacion(EstacionDelAnio estacion) {
+		this.estacion = estacion;
+	}
+
+	public Set<Espacio> getTiposEspacios() {
+		return tiposEspacios;
+	}
+
+	public void setTiposEspacios(Set<Espacio> tiposEspacios) {
+		this.tiposEspacios = tiposEspacios;
+	}
+
+	/* Fin Getters and Setters */
+
 }
---
+++ src/modulos/Test.java
@@ -0,0 +1,16 @@
+package modulos;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+		Date date = new Date();
+		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		int year  = localDate.getYear();
+		int month = localDate.getMonthValue();
+		int day   = localDate.getDayOfMonth();
+		System.out.println("Dia "+day+" Mes: "+month+" Año: "+ year);
+	}
+}
